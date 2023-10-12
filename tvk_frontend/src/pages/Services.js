decbfabda98d84ced2dcc76903682450544d531d
Anjali Anupam <2019024@iiitdmj.ac.in>
2023-10-12 22:38:21 +0530

    create services

---
+++ tvk_frontend/src/pages/Services.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import './Services.css';
+
+const Services = () => {
+  return (
+    <div>
+        <div className='services_maincontainer'>
+            <div className='services_heading'>
+                <div className='services_heading_text'>
+                    Want to do something different to find satisfaction and fulfilment at work?
+                </div>
+
+                <div className='services_subcontainer'>
+                    <div className='services_target'>CAREER</div>
+                    
+                    <div className='services_card_container'>
+
+                        <div className='services_card_row1'>
+                            <div className='services_card'>
+                                <div className='services_card_heading'>CAREER CHANGE</div>
+                                <div className='services_card_text'>Difficulty in finding satisfaction
+                                and fulfilment at work</div>
+                            </div>
+                            <div className='services_card'>
+                                <div className='services_card_heading'>CAREER CHANGE</div>
+                                <div className='services_card_text'>Difficulty in finding satisfaction
+                                and fulfilment at work</div>
+                            </div>
+                        </div>
+
+                        <div className='services_card_row2'>
+                            <div className='services_card'>
+                                <div className='services_card_heading'>CAREER CHANGE</div>
+                                <div className='services_card_text'>Difficulty in finding satisfaction
+                                and fulfilment at work</div>
+                            </div>
+                            <div className='services_card'>
+                                <div className='services_card_heading'>CAREER CHANGE</div>
+                                <div className='services_card_text'>Difficulty in finding satisfaction
+                                and fulfilment at work</div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Services
