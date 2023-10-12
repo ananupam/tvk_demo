fa67af684006ea748839405d39152b9fae018ed5
Anjali Anupam <2019024@iiitdmj.ac.in>
2023-10-12 22:37:00 +0530

    create newsletter

---
+++ tvk_frontend/src/pages/Newsletter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Newsletter.css'
+
+const Newsletter = () => {
+  return (
+    <div>
+        <div className='newsletter_maincontainer'>
+            <div className='background-layer'>
+                <div className="upper-layer"><h1>1</h1></div>
+                <div className="bottom-layer"><h1>2</h1></div>
+            </div>
+        </div>
+
+        
+    </div>
+  )
+}
+
+export default Newsletter
