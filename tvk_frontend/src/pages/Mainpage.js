7f50b322ebded2ad6d0d0fd3c586b65e13179229
Anjali Anupam <2019024@iiitdmj.ac.in>
2023-10-12 22:36:46 +0530

    create mainpage

--- tvk_frontend/src/pages/Mainpage.js
+++ tvk_frontend/src/pages/Mainpage.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './blobanimation.svg'; 
+import './Mainpage.css'
+
 
 const Mainpage = () => {
   return (
     <div>
-        <div className='mainpage_container'>
-            <div className='mainpage_text'>
-                <h2>KRISHNA CHATURVEDI</h2>
-                <h4>Career Strategiest | Career Mentor | Growth Hacker | Dream Job Coach | Lead Recruiter </h4>
+      <div className='mainpage_container'>
+
+        <div className='mainpage_text_container'>
+          <div className='mainpage_heading'>KRISHNA CHATURVEDI</div>
+          <div className='mainpage_subheading'>Career Strategist | Mentor | Growth Hacker | Job Coach  </div>
+          <button className='mainpage_consult'>CONSULT</button>
         </div>
+
+        <div className='mainpage_image_container'>
+          <img src={require('./blobanimation.svg').default} alt="Blob SVG" />
+          <div className='mainpage_image_bkg1'></div>
+        </div>
+
+      </div>
     </div>
-</div>
   )
 }
+
 
 export default Mainpage
