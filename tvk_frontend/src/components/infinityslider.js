15d0cf22e9bb7988a92285ccdd1d3963d0a6983d
Anjali Anupam <2019024@iiitdmj.ac.in>
2023-10-12 22:38:09 +0530

    create infinity slider

---
+++ tvk_frontend/src/components/infinityslider.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './infinityslider.css';
+
+const InfinitySlider = () => {
+  return (
+    <div>
+      <div className="conclusion">
+        <ul className="dynamictext">
+          <li><span> SLIDER 1</span></li> 
+          <li><span>SLIDER 2</span>  </li> 
+          <li><span> SLIDER 3</span></li>
+        </ul>
+      </div>
+    
+
+      
+    </div>
+  )
+}
+
+export default InfinitySlider
